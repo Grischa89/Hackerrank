d60ccc17e41bace1e120dd9470e8311b028e0d28
Grischa89 <43987096+Grischa89@users.noreply.github.com>
2019-02-22 20:13:37 +0100

    Update Weather Observation Station 15.sql

--- Weather Observation Station 15.sql
+++ Weather Observation Station 15.sql
@@ -1,13 +1,13 @@
-https://www.hackerrank.com/challenges/weather-observation-station-15/problem
+--https://www.hackerrank.com/challenges/weather-observation-station-15/problem
 
-Solution 1
+--Solution 1
 
 SELECT ROUND(`LONG_W`, 4) FROM `STATION` 
 WHERE `STATION`.`LAT_N` < 137.2345
 ORDER BY `LAT_N` DESC
 LIMIT 1
 
-Solution 2
+--Solution 2
 
 SELECT CAST(LONG_W AS DECIMAL(10,4)) FROM STATION
     WHERE LAT_N = 
